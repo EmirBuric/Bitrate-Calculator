eef43bffe9bf005846040b46c5495cd329123e1f
EmirBuric <160528544+EmirBuric@users.noreply.github.com>
2025-04-15 22:21:07 +0200

    Added a Calculator

---
+++ Bitrate Calculator/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitrate_Calculator
+{
+    public class Calculator
+    {
+        private string _x1;
+        private string _x2;
+        public Calculator(string x1,string x2) 
+        {
+            _x1 = x1;
+            _x2 = x2;
+        }
+
+        public void Calc() 
+        {
+            uint ux1;
+            uint ux2;
+
+            int x1=0;
+            int x2=0;
+
+            if (uint.TryParse(_x1, out ux1)) 
+            {
+                x1 = (int)ux1;
+            }
+            if (uint.TryParse(_x2, out ux2))
+            {
+                x2 = (int)ux2;
+            }
+
+            if (x1<x2)
+            {
+                int delta=x2-x1;
+                int delta_bit = delta * 8;
+                double bitrate = delta_bit / 0.5;
+                double bitrate_shorten = bitrate / 1000000;
+                Console.WriteLine($"Bitrate: {bitrate_shorten} Mbit/s");
+            }
+        }
+
+
+    }
+}
--- Bitrate Calculator/Program.cs
+++ Bitrate Calculator/Program.cs
@@ -8,8 +8,19 @@ DeviceData items=jsonMethods.LoadJson();
 Console.WriteLine($"Device: {items.Device}");
 Console.WriteLine($"Model: {items.Model}");
 
-for (int i = 0;i<items.NIC.Count;i++)
+Console.WriteLine($"Descrition: {items.NIC[0].Description}");    
+Console.WriteLine($"MAC Adress: {items.NIC[0].MAC}");
+
+
+for (int i = 1;i< items.NIC.Count;i++)
 {
-    Console.WriteLine($"Descrition: {items.NIC[i].Description}");    
-    Console.WriteLine($"MAC Adress: {items.NIC[i].MAC}");      
+    Calculator calculatorTx = new Calculator(items.NIC[i-1].Tx, items.NIC[i].Tx);
+    Calculator calculatorRx = new Calculator(items.NIC[i-1].Rx, items.NIC[i].Rx);
+    Console.WriteLine("Tx bitrate: ");
+    calculatorTx.Calc();
+    Console.WriteLine("Rx bitrate: ");
+    calculatorRx.Calc();
 }
+
+
+
