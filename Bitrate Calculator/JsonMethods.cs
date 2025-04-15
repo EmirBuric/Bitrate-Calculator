fb6aace2037c3e861808756ceb9db8fac38a82c1
EmirBuric <160528544+EmirBuric@users.noreply.github.com>
2025-04-15 21:00:56 +0200

    Parse JSON

---
+++ Bitrate Calculator/JsonMethods.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitrate_Calculator
+{
+    public class JsonMethods
+    {
+        public DeviceData LoadJson()
+        {
+            StreamReader r = new StreamReader("Data.json");
+            string json = r.ReadToEnd();
+            DeviceData items = JsonConvert.DeserializeObject<DeviceData>(json);
+            return items;
+
+        }
+        public void Read()
+        {
+
+        }
+    }
+}
---
+++ Bitrate Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Bitrate_Calculator;
+
+
+JsonMethods jsonMethods = new JsonMethods();
+DeviceData items=jsonMethods.LoadJson();
+
+Console.WriteLine($"Device: {items.Device}");
+Console.WriteLine($"Model: {items.Model}");
+
+for (int i = 0;i<items.NIC.Count;i++)
+{
+    Console.WriteLine($"Descrition: {items.NIC[i].Description}");    
+    Console.WriteLine($"MAC Adress: {items.NIC[i].MAC}");      
+}
