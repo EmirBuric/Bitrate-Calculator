7297317136f9f2a30d4b2ec77428db784ff7c8a3
EmirBuric <160528544+EmirBuric@users.noreply.github.com>
2025-04-16 12:11:59 +0200

    Added a delay and moved functions from Program to WriteData class

--- Bitrate Calculator/Program.cs
+++ Bitrate Calculator/Program.cs
@@ -1,32 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Bitrate_Calculator.Interfaces;
 using Bitrate_Calculator.Models;
 using Bitrate_Calculator.Services;
 using System.Globalization;
 
 
-JsonMethods jsonMethods = new JsonMethods();
-DataStrings itemStrings=jsonMethods.LoadJson();
 
-DeviceData items= jsonMethods.ManualParse(itemStrings);
+WriteData writeData = new WriteData();
 
-Console.WriteLine($"Device: {items.Device}");
-Console.WriteLine($"Model: {items.Model}");
-
-Console.WriteLine($"Descrition: {items.NIC[0].Description}");    
-Console.WriteLine($"MAC Adress: {items.NIC[0].MAC}");
-
-Calculator calculatorTx = new Calculator();
-Calculator calculatorRx = new Calculator();
-
-for (int i = 1;i< items.NIC.Count;i++)
-{
-    string currentTime =items.NIC[i].Timestamp.ToString("dd.MM.yyyy. HH:mm:ss.fff");
-    Console.WriteLine($"Current time: {currentTime}");
-    Console.WriteLine("Tx bitrate: ");
-    calculatorTx.Calc(items.NIC[i - 1].Tx, items.NIC[i].Tx, items.NIC[i - 1].Timestamp, items.NIC[i].Timestamp);
-    Console.WriteLine("Rx bitrate: ");
-    calculatorRx.Calc(items.NIC[i - 1].Rx, items.NIC[i].Rx, items.NIC[i - 1].Timestamp, items.NIC[i].Timestamp);
-}
+writeData.Write();
 
 
 
---
+++ Bitrate Calculator/Services/WriteData.cs
@@ -0,0 +1,51 @@
+﻿using Bitrate_Calculator.Interfaces;
+using Bitrate_Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitrate_Calculator.Services
+{
+    public class WriteData : IWriteData
+    {
+        public void Write()
+        {
+            JsonMethods jsonMethods = new JsonMethods();
+            DataStrings itemStrings = jsonMethods.LoadJson();
+
+            DeviceData items = jsonMethods.ManualParse(itemStrings);
+
+            Console.WriteLine($"Device: {items.Device}");
+            Console.WriteLine($"Model: {items.Model}");
+
+            Console.WriteLine($"Descrition: {items.NIC[0].Description}");
+            Console.WriteLine($"MAC Adress: {items.NIC[0].MAC}");
+
+            Calculator calculatorTx = new Calculator();
+            Calculator calculatorRx = new Calculator();
+
+            for (int i = 1; i < items.NIC.Count; i++)
+            {
+                string currentTime = items.NIC[i].Timestamp.ToString("dd.MM.yyyy. HH:mm:ss.fff");
+                Console.WriteLine($"Current time: {currentTime}");
+                Console.WriteLine("Tx bitrate: ");
+                calculatorTx.Calc(items.NIC[i - 1].Tx, items.NIC[i].Tx, items.NIC[i - 1].Timestamp, items.NIC[i].Timestamp);
+                Console.WriteLine("Rx bitrate: ");
+                calculatorRx.Calc(items.NIC[i - 1].Rx, items.NIC[i].Rx, items.NIC[i - 1].Timestamp, items.NIC[i].Timestamp);
+                if (i < items.NIC.Count - 1)
+                {
+                    ClearCurrentData();
+                }
+
+            }
+        }
+        private static void ClearCurrentData()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, currentLineCursor - 5);
+            Thread.Sleep(1000);
+        }
+    }
+}
